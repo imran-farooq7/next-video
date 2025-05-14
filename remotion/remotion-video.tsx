09854fb028685c51a230b593629b4b860208364d
imran farooq <imran_farooq7@hotmail.com>
2025-05-14 21:11:38 +0500

    audio intergrated

--- remotion/remotion-video.tsx
+++ remotion/remotion-video.tsx
@@ -1,14 +1,30 @@
 import { VideoContext } from "@/context/video";
 import { useContext } from "react";
-import { AbsoluteFill, Img, Sequence, useVideoConfig, Audio } from "remotion";
+import {
+  AbsoluteFill,
+  Img,
+  Sequence,
+  useVideoConfig,
+  Audio,
+  useCurrentFrame,
+} from "remotion";
 const RemotionVideo = () => {
   const { audio, captions, images } = useContext(VideoContext)!;
   const { fps } = useVideoConfig();
+  const frame = useCurrentFrame();
   // Calculate total duration based on captions
   const totalDuration =
     captions.length > 0
       ? Math.ceil((captions[captions.length - 1] as any).end / (1000 / 30)) + 30 // Add 30 frames for an additional second
       : 1; // Default to 1 second if no captions
+  const currentCaption = () => {
+    const currentTime = (frame / fps) * 1000;
+    const currentCaption = captions.find(
+      (caption: any) =>
+        currentTime >= caption.start && currentTime <= caption.end
+    );
+    return currentCaption ? (currentCaption as any).text : "";
+  };
   return (
     <AbsoluteFill>
       {images.map((img, i) => (
@@ -26,6 +42,17 @@ const RemotionVideo = () => {
               margin: "auto",
             }}
           />
+          <AbsoluteFill
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+            }}
+          >
+            <h2 className="text-white text-center text-4xl">
+              {currentCaption()}
+            </h2>
+          </AbsoluteFill>
         </Sequence>
       ))}
       <Audio src={audio} />
