e47a1c32db130d4377ff4c07d262370d472caeaf
imran farooq <imran_farooq7@hotmail.com>
2025-05-29 03:33:53 +0500

    credits saving action done

---
+++ actions/credits.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import prisma from "@/prisma/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+export const saveCredits = async (amount: number, credits: number) => {
+  try {
+    const user = await currentUser();
+    const userEmail = user?.emailAddresses[0]?.emailAddress;
+    const existingCredit = await prisma.credit.findFirst({
+      where: {
+        userEmail: userEmail!,
+      },
+    });
+    if (existingCredit) {
+      existingCredit.amount += amount;
+      existingCredit.credits += credits;
+      await prisma.credit.update({
+        where: { userEmail: existingCredit.userEmail },
+        data: existingCredit,
+      });
+      return {
+        success: true,
+        message: "Credits updated successfully",
+      };
+    } else {
+      await prisma.credit.create({
+        data: {
+          userEmail: userEmail!,
+          amount,
+          credits,
+        },
+      });
+      return {
+        success: true,
+        message: "Credits saved successfully",
+      };
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
