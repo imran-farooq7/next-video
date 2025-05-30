d27f8883a0c8e1c5b9fcab324582b16d4b27efb9
imran farooq <imran_farooq7@hotmail.com>
2025-05-31 00:51:56 +0500

    credits render in navbar

--- components/nav/credits.tsx
+++ components/nav/credits.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import { VideoContext } from "@/context/video";
+import { useContext, useState } from "react";
 
 const Credits = () => {
-  const [total, setTotal] = useState(0);
+  const ctx = useContext(VideoContext);
+  const { credits } = ctx!;
+  const [total, setTotal] = useState(credits);
   return (
     <div className="bg-white rounded-2xl px-8 py-2 text-emerald-500 font-semibold">
-      Credits:{total}
+      Credits:{credits}
     </div>
   );
 };
